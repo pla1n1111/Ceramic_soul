3244e1bf0974ace55995e0e955ef3a2fd16b011d
Anton Slizhov <sonmil.100@gmail.com>
2025-04-07 15:05:30 +0300

    1.01 with footer, menu and second page

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,21 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import "/src/sass/style.scss";
 
-const swiper = new Swiper('.swiper', {
+const burger = document.querySelector(".burger"),
+	close = document.querySelector(".header__menu-close"),
+	menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+	menu.classList.add("header__menu_active");
+	document.body.style.overflow = "hidden";
+});
+close.addEventListener("click", () => {
+	menu.classList.remove("header__menu_active");
+	document.body.style.overflow = "";
+});
+
+try {
+    const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     loop: true,
     pagination: {
@@ -28,3 +42,26 @@ const swiper = new Swiper('.swiper', {
     },
     modules: [Navigation, Pagination],
 });
+} catch(e) {}
+
+try {
+	const tabs = document.querySelectorAll(".catalog__tab");
+	const contents = document.querySelectorAll(".catalog__content-item");
+
+	tabs.forEach((tab, index) => {
+		tab.addEventListener("click", () => {
+			// Удаляем активный класс у всех табов и контента
+			tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
+			contents.forEach((c) => (c.style.display = "none"));
+
+			// Добавляем активный класс к нажатому табу и показываем соответствующий контент
+			tab.classList.add("catalog__tab_active");
+			contents[index].style.display = "flex";
+		});
+	});
+
+	// Показываем первый контент при загрузке
+	contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
+} catch (e) {}
+
+// Обратите внимание, что значение block (в двух местах) можно спокойно поменять на flex, если вам это необходимо
